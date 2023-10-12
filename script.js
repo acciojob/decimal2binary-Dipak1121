1241669695aa8a6b8d485b20bc102ce5875f7a16
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-12 16:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  let ans = "";
+	while ( num != 0 ){
+		ans = (num%2) + ans;
+		num = Math.floor(num/2);
+	}
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
